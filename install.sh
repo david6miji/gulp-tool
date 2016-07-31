2463643655d568dee162db90b7ee79f017e88edc
frog <frog@falinux.com>
2016-07-31 15:49:36 +0900

    test npm install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo docker pull david6miji/gulp-tool
-sudo cp -a gtool       /usr/bin/gtool
-sudo cp -a gtool-dev   /usr/bin/gtool-dev
+sudo cp -a gulp-tool       /usr/bin/
+sudo cp -a gulp-tool-dev   /usr/bin/
 
+gulp-tool "npm install"

f1a1fe12de08c2c894dcb274fafd069831da43b3
root <david6miji@gmail.com>
2016-08-14 13:51:13 +0000

    backup

---
+++ gulp_tasks/docker/pull-david6miji.js
@@ -0,0 +1,18 @@
+var 
+
+    gulp				= require('gulp'),
+	
+end_require= true;
+
+gulp.task('docker:pull:david6miji', function() {
+	
+	console.log( '-------------------------------------------------------------------' );
+	console.log( '>> docker pull david6miji.' );
+	console.log( '-------------------------------------------------------------------' );
+	
+	// git reset --hard HEAD
+	// git pull
+	// 
+	
+});
+

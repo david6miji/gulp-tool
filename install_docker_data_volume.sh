a3cce64af3b9eba93a8795cf91a5d87a836ccc39
root <david6miji@gmail.com>
2016-08-15 04:53:00 +0000

    backup

---
+++ install_docker_data_volume.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ~/gulp-tool/
+mkdir -p ~/gulp-env/
+
+docker rm gulp-tool-dir
+docker create --name gulp-tool-dir \
+       -v ~/gulp-tool:/gulp-tool/ \
+       -v ~/gulp-env:/gulp-env \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v /:/gulp-host \
+       busybox /bin/true

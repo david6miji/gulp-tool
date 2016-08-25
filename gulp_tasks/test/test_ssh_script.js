dd4e6b85533a7f6cb442a336208f59a98079689b
david6miji <david6miji@gmail.com>
2016-08-25 16:08:00 +0000

    backup

--- gulp_tasks/test/test_ssh_script.js
+++ gulp_tasks/test/test_ssh_script.js
@@ -1,28 +1,30 @@
-var 
+var
 
     gulp				= require('gulp'),
 	SSHScript			= require(__dirname + '/../../lib/test.js'),
-	
+
 end_require= true;
 
 gulp.task('test_ssh_script', function(done) {
-	
+
 	console.log( '-------------------------------------------------------------------' );
 	console.log( '>> run test ssh script.' );
 	console.log( '-------------------------------------------------------------------' );
-	
+
 	var script = new SSHScript();
-	
+	var test_env_name = 'withme';
 	script.begin()
-		.ip			( "127.0.0.1" )
-		.port		( 22 )
-		.username   ( gt_env[gt_env_name].host.ssh.username )
-		.password   ( gt_env[gt_env_name].host.ssh.password )
+		.ip			( gt_env[test_env_name].host.ssh.ip )
+		.port		( gt_env[test_env_name].host.ssh.port )
+		.username   ( gt_env[test_env_name].host.ssh.username )
+		.password   ( gt_env[test_env_name].host.ssh.password )
 		.connect    ()
+        .run        ('pwd')
+        .run        ('cd ~/gulp-tool/')
+        .run        ('pwd')
 		.run        ('ls -al')
-		
+
 	.end(done);
-	
+
 //	done();
 });
-

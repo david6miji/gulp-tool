476cc50dfbaa64347a2d356a0c26f2c2e4d5762d
david6miji <david6miji@gmail.com>
2016-08-27 11:03:03 +0000

    backup

--- gulp_tasks/test/test_ssh_script.js
+++ gulp_tasks/test/test_ssh_script.js
@@ -13,17 +13,28 @@ gulp.task('test_ssh_script', function(done) {
 
 	var script = new SSHScript();
 	var test_env_name = 'withme';
-	script.begin()
+	script
+    .begin()
 		.ip			( gt_env[test_env_name].host.ssh.ip )
 		.port		( gt_env[test_env_name].host.ssh.port )
 		.username   ( gt_env[test_env_name].host.ssh.username )
 		.password   ( gt_env[test_env_name].host.ssh.password )
-		.connect    ()
-        .run        ('pwd')
-        .run        ('cd ~/gulp-tool/')
-        .run        ('pwd')
-		.run        ('ls -al')
-
+	.connect    ()
+    .shell      ()
+        .wait       ('frog@withme2020:~$')
+//        .wait       ('frog@withme2020:~$', timeout, function)
+        .input      ('ls -al\n')
+        .wait       ('frog@withme2020:~$')
+//        .is_error    ( function )
+        .input      ('pppsdfksdjf\n')
+        .wait       ('frog@withme2020:~$')
+        .input      ('exit\n')
+//        .run        ('pwd')
+//        .run        ('cd ~/gulp-tool/')
+//        .run        ('pwd')
+//		.run        ('ls -al')
+//		.exec        ('ls -al')
+//    .exit(0)
 	.end(done);
 
 //	done();

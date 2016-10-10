ce9b64a7b73b905d9f26ac1de06e533a6d304333
david6miji <david6miji@gmail.com>
2016-10-10 10:37:18 +0000

    backup

--- bin/win/gulp-tool.bat
+++ bin/win/gulp-tool.bat
@@ -1,4 +1,4 @@
 @ECHO off
 ECHO RUN gulp-tool ...
 ECHO %1 %2 %3 %4
-docker run -it --rm --volumes-from gulp-tool-dir -v %CD%:/work david6miji/gulp-tool:latest %1 %2 %3 %4
+docker run -it --rm --network=host --volumes-from gulp-tool-dir -v %CD%:/work david6miji/gulp-tool:latest %1 %2 %3 %4
